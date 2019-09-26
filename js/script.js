44795a00fee46aed19093e740e1d4ca847c43ed1
Lucas Gabriel <buscoplay@gmail.com>
2019-09-26 13:19:12 -0300

    mundial 2019

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function checkCK(){
+    var ck = document.getElementById("check");
+    var icon = document.getElementById("menu");
+    if (ck.checked == true){
+        ck.checked = false;
+    }
+    else{
+        ck.checked = true;
+    }
+}
